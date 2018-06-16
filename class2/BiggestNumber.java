bc34e913060a991e22a3efe37d1d8faf4b2df532
raspreet kaur <kaurraspreet125@gmail.com>
2018-06-15 17:37:21 -0700

    commands added

---
+++ class2/BiggestNumber.java
@@ -0,0 +1,22 @@
+ class BiggestNumber{
+
+public static void main(String[] args){
+
+int a=10;
+int b=34;
+int c=30;
+
+if(a>b)&&(a>c){
+System.out.println(a+"a is greatest");
+} 
+else{
+if (b>a)&&(b>c){
+System.out.println(b+"b is greatest");
+}
+else{
+System.out.println(c+"c is greatest");
+}
+
+}
+
+}
