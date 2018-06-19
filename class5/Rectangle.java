04fcc8d8a421c1e8e62041bd21f10b546708ba32
raspreet kaur <kaurraspreet125@gmail.com>
2018-06-19 13:06:13 -0700

    correction is done

--- class5/Rectangle.java
+++ class5/Rectangle.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 class Rectangle{
 
 int length,breadth;
-public int CalculateArea(int length,int breadth){
+public int CalculateArea(int l,int b){
 
-return length*breadth;
+return l*b;
 }
 public static void main(String[] args){
 Scanner sc;
 sc=new Scanner(System.in);
 System.out.println("enter length and breadth");
-int l= sc.nextInt();
-int b= sc.nextInt();
+int length= sc.nextInt();
+int breadth= sc.nextInt();
 Rectangle r1=new Rectangle();
-int a=r1.CalculateArea(l,b);
-System.out.println(a);
+int a=r1.CalculateArea(length,breadth);
+System.out.println( "the area of rectangle is "+a);
 }
 }
